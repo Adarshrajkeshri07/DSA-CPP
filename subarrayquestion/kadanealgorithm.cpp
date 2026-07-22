1ba2e86486bbea118a3e60767a08a07090ce7584
Adarsh Raj Keshri <rajadarshkeshri@gmail.com>
2026-07-22 14:28:56 +0530

    kadane's algorithm

--- subarrayquestion/kadanealgorithm.cpp
+++ subarrayquestion/kadanealgorithm.cpp
@@ -3,15 +3,15 @@
 using namespace std ; 
 int main(){
     int a[5] = {-1,-2,-6,-9,-8};
-    int maxsum = INT_MIN ; 
+    int maxsum = INT_MIN ;
+    int sum = 0;
     for(int i = 0 ; i < 5 ; i++){
-        int sum = 0 ; 
-        for(int j = i ; j < 5 ;j++){
-            sum+=a[j];
-            maxsum = max(sum , maxsum) ;
+       
+         sum+=a[i];
+        maxsum = max(sum , maxsum) ;
             if(sum < 0 ) {
                 sum = 0 ;
-            }
+            
         }
     }
     cout << maxsum ;
