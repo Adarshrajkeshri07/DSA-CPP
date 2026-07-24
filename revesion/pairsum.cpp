7bf4b6fec5921b7fcef9b134aa858a5eb8298332
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-07-24 15:35:36 +0530

    new concept in pair sum wwhen user decide input

--- revesion/pairsum.cpp
+++ revesion/pairsum.cpp
@@ -1,4 +1,4 @@
-// #include<iostream>
+// #include<iostream>// brute force se 
 // using namespace std ; 
 // int main(){
 //     int arr[4] = { 2,7,11,15} ; 
@@ -22,6 +22,7 @@
 //     return 0 ; 
 
 // }
+// two pointer se ..
 // #include<iostream>
 // #include<vector>
 // using namespace std ; 
@@ -61,3 +62,50 @@
 //     pairsum(nums ,target ,nums.size() );
 //     return 0 ; 
 // }
+// two pointer using vector style ... 
+#include<iostream>
+#include<vector>
+using namespace std ; 
+vector<int> pairsum(vector<int> a , int target , int size){
+    vector<int> ans ; 
+    int st = 0 , end = size - 1 ,pairsum = 0   ; 
+    while(st < end){
+        pairsum = a[st] + a[end] ; 
+        if(pairsum < target){
+            st++ ; 
+        }else if
+             (pairsum > target)
+            {
+                end--;
+            }
+            else{
+                ans.push_back(st) ;
+                ans.push_back(end) ;
+                return ans ;
+            }
+    }
+    return ans ; 
+   
+}
+int main(){
+    int n ; 
+    cout << " enter the value of   n : " ; 
+    cin >> n ;
+    vector<int> nums (n); 
+    cout <<  " enter the value of element : " ; 
+    for(int &val : nums){
+        cin >> val ; 
+    }
+    int target ; 
+    cout << " enter the value of target : " ; 
+    cin >> target ; 
+    vector<int> ans ; 
+   ans =  pairsum(nums,target,n);
+ if(ans.size()==2){
+    cout << ans[0] << ans[1] ; 
+ }else{
+    cout << " pair sum doent matched " ; 
+ }
+ return 0 ; 
+
+}
