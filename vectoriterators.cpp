32a22c15145c664edb5e3f39357e345d21cae29e
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-08-27 17:27:44 +0530

    iterator vector

---
+++ vectoriterators.cpp
@@ -0,0 +1,46 @@
+// vector
+// vec.begin () ;  // first element of the vector ..
+// vec.end () ; // end + 1 element of the vector ..... because {]
+// its work like pointer .........using * before it
+
+//  #include<iostream>
+//  #include<vector>
+//  using namespace std ;
+//  int main(){
+//     vector<int>  vec = {1,2,3,4,5} ;
+//     cout << *vec.begin () << endl ;
+//     cout << *vec.end () ; // its give occured value or end value means end + 1 . that not exist in the vector 5 is end - 1 element ...
+//  }
+
+// iterator in vector 
+// iterator is pointer ............*iterator is what at the iterator ...
+
+// forward iterator ...............
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int> vec = {1, 2, 3, 4, 5};
+//     vector<int>::iterator itr; // iterator is inbuilt or itr is its user define name ........
+//     for (itr = vec.begin(); itr < vec.end(); itr++)
+//     { // != generally ye use hota hai .....
+//         cout << *itr << " ";
+//     }
+//     return 0;
+// }
+
+
+
+
+//backword iterators ...........
+
+
+
+
+
+// using function 
+// 1 : -  vec.rbegin() ; <> opposite of vec.begin() ; 
+// 2 : -  vec.rend() ; <> opposite of vec.end() ; 
