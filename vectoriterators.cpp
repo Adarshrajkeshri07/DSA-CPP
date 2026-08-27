29bdf52586d540ab9cccf4519234cd2c9f0912c4
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-08-27 17:37:43 +0530

    use of auto

--- vectoriterators.cpp
+++ vectoriterators.cpp
@@ -52,13 +52,45 @@
 // 2 : -  vec.rend() ; <> opposite of vec.end() ; 
 
 
-#include<iostream>
-#include<vector>
-using namespace std ; 
-int main(){
-    vector<int> vec = {1,2,3,4,5} ; 
-    vector<int> :: reverse_iterator it ; 
-    for(it = vec.rbegin() ; it != vec.rend() ; it++){
-        cout << *it << " " ; 
+// #include<iostream>
+// #include<vector>
+// using namespace std ; 
+// int main(){
+//     vector<int> vec = {1,2,3,4,5} ; 
+//     vector<int> :: reverse_iterator it ; 
+//     for(it = vec.rbegin() ; it != vec.rend() ; it++){
+//         cout << *it << " " ; 
+//     }
+// }
+
+//original method 
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int> vec = {1, 2, 3, 4, 5};
+
+//     for (vector<int>::reverse_iterator  it = vec.rbegin(); it != vec.rend(); it++)
+//     {
+//         cout << *it << " ";
+//     }
+// }
+
+
+
+
+//   vector<int>::reverse_iterator it; we use instead of this == auto
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> vec = {1, 2, 3, 4, 5};
+
+    for (auto it = vec.rbegin(); it != vec.rend(); it++)
+    {
+        cout << *it << " ";
     }
 }
