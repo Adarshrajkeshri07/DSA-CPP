374009802689f05c5a36acf6bee73773cc7104c3
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-08-30 15:50:40 +0530

    input in strings

--- Strings/introductionofstring/strings/basic.cpp
+++ Strings/introductionofstring/strings/basic.cpp
@@ -2,9 +2,21 @@
 #include<string>
 using namespace std ; 
 int main(){
-    string str = "adarshrajkeshri" ; // runtime reszie dynamically 
-    cout << str << endl ;
-    str = "adarshraj" ; // because dynamically in nature but arrays has fix and constant size ....
+    // string str = "adarshrajkeshri" ; // runtime reszie dynamically 
+    // cout << str << endl ;
+    // str = "adarshraj" ; // because dynamically in nature but arrays has fix and constant size ....
+    // cout << str;
+    string str = "adarsh" ; 
+    string str2 = "raj" ; 
+    string str3 = str + str2 ; 
+    // cout << str3 ; 
+    // cout << str3.length() << endl ; 
+    // cout <<( str == str2 ) << endl  ; //0 ;
+    string a ; 
+    cout << "enter: " ; 
+    // cin >> a ; 
+    getline(cin , a) ;
+    cout << a ; 
 
-    cout << str;
+   
 }
