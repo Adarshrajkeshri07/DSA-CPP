9bed3c2d99b5597bf089679845b0b3009b0c7e39
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-09-01 22:40:44 +0530

    removealloccurrence.cpp

---
+++ Strings/stringquestion/removealloccurrences.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std ; 
+int main(){
+    string s = "daabcabcabcabcbc";
+    string part = "abc" ; 
+while(s.length() > 0 && s.find(part) < s.length() ){
+        int pos = s.find(part);
+        if (pos < s.length())
+        {
+            s.erase(pos, part.length());
+        }
+    }
+   
+    cout << s ; 
+    return 0 ; 
+}
