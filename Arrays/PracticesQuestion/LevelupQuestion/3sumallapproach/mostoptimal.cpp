1f7945d5ce39a0a334c95b134e9819efd8807e91
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-09-19 18:51:43 +0530

    best approach of three sum problem

--- Arrays/PracticesQuestion/LevelupQuestion/3sumallapproach/mostoptimal.cpp
+++ Arrays/PracticesQuestion/LevelupQuestion/3sumallapproach/mostoptimal.cpp
@@ -1,26 +1,42 @@
-#include<iostream>
-#include<set>
-#include<algorithm>
-using namespace std ; 
-int main(){
-    int a[6] = {-1,0,1,2,-1,-4} ; 
-    int n = 6 ; 
-    sort(a , a+n) ; 
-    for(int i = 0 ; i < n ; i++){
-        int first = a[i] ;
-        int j = i + 1 , k = n - 1 ; 
-        while(j < k ){
-           int sum = a[i] + a[j] + a[k] ;
-           if (sum == 0)
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {-1, 0, 1, 2, -1, -4};
+    int n = 6;
+    sort(arr, arr + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && arr[i] == arr[i - 1])
+            continue;
+        int a = arr[i];
+        int j = i + 1, k = n - 1;
+        while (j < k)
+        {
+            int sum = a + arr[j] + arr[k];
+            if (sum < 0)
             {
-                cout << a[i] << a[j] << a[k] ; 
-                return 0 ;
+                j++;
             }
-            else if(sum > 0 ){
-                k-- ; 
-            }else{
-                j++ ; 
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                
+                cout << arr[i] << " " << arr[j] << " " << arr[k] << "\n";
+
+                int val_j = arr[j], val_k = arr[k];
+                while (j < k && arr[j] == val_j)
+                    j++;
+                while (j < k && arr[k] == val_k)
+                    k--;
             }
         }
     }
+    return 0;
 }
