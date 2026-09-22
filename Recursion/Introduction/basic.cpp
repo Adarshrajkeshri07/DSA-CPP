9a8cc4832c863571f5bce9058cbf17a5641ee597
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-09-22 22:10:51 +0530

    introcuion of recusrion

---
+++ Recursion/Introduction/basic.cpp
@@ -0,0 +1,25 @@
+// ==================== RECURSION ====================
+
+// Recursion:
+// Jab ek function khud ko hi call karta hai.
+
+// Recursion ke 2 main parts:
+// 1. Base Case      -> recursion ko stop karta hai
+// 2. Recursive Case -> function khud ko dobara call karta hai
+
+// Base Case nahi hoga -> Infinite Recursion -> Stack Overflow
+
+// Recursion me problem ko chhote/simpler problem me break karte hain.
+
+// Har recursive call Call Stack me store hoti hai.
+
+// Example:
+// factorial(5) = 5 * factorial(4)
+// factorial(4) = 4 * factorial(3)
+// ...
+// factorial(1) = 1   <- Base Case
+
+// Important:
+// Base Case = STOP
+// Recursive Case = REPEAT
+
---
+++ Recursion/Introduction/example.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ; 
+void printnumber(int n){
+   if(n == 1) {cout << n ; return  ; }
+   cout << n << " " ;
+   printnumber(n-1) ;     
+}
+int main(){
+    int n = 10 ; 
+    printnumber(n) ; 
+    return 0 ; 
+}
